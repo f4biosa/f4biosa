9b5852cdea8d39102e09dd1e89b4701689b13f4a
Fabio Santos <f4bio.sa@gmail.com>
2022-10-22 14:12:27 -0400

    Ajustes para o bootstrap 5.2

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
-import '../styles/bootstrap.css';
+import Head from 'next/head'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/custom.css';
-import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <main>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+        <Component {...pageProps} />
+      </main>
+  );
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,69 +1,32 @@
-import Head from 'next/head'
-//import styles from '../styles/Home.module.css'
-
 export default function Home() {
 return (
 <div>
-    <Head>
-        <title>f4bio.dev</title>
-        <meta name="description" content="Developer Home Page" />
-    </Head>
-    <main>
-        <div className="container">
-            <div className="row">
-
-                <div className="col-xs-12 text-align-center">
-                    <h1 className='hero-title'>
-                        Olá, me chamo Fábio Santos!
-                    </h1>
-
-                    <p className='hero-description'>
-                        Esse espaço foi criado para divulgar minhas redes sociais e os trabalhos que realizo como desenvolvedor.
-                    </p>
-                </div>
+    <div className="px-4 py-5 my-5 text-center">
+            <h1 className="display-5 fw-bold"> Olá, me chamo Fábio Santos!</h1>
+            <div className="col-lg-6 mx-auto">
+                <p className="lead mb-4">Esse espaço foi criado para divulgar minhas redes sociais e os trabalhos que realizo como desenvolvedor.</p>
             </div>
-        
-        <div className="row">
-            <div className="col-lg-6 col-xs-12">
-                <div className="card">
-                    <div className="card-body">
-                        <h2 className="card-title"><a href="https://github.com/f4biosa" >/f4biosa no GitHub &rarr;</a></h2>
-                        <p className="card-text">Aqui você encontra meus repositórios de código-fonte</p>
-                    </div>
+    </div>
+    <div className="container px-2 py-5" id='feature-1'>
+        <div className="row g-4 py-5 row-cols-1 row-cols-lg-2">
+                <div className="feature col">
+                    <h6 className="fs-2"><a href="https://github.com/f4biosa" >/f4biosa</a> no GitHub &rarr;</h6>
+                    <p className="card-text">Aqui você encontra meus repositórios de código-fonte</p>
                 </div>
+            <div className="feature col">
+                <h6 className="fs-2"><a href="https://twitter.com/f4bio_sa" >@f4bio_sa</a> no Twitter &rarr;</h6>
+                <p className="card-text">Apenas acompanho alguns assuntos polêmicos</p>
             </div>
-            <div className="col-lg-6 col-xs-12">
-                <div className="card">
-
-                    <div className="card-body">
-                        <h2 className="card-title"><a href="https://twitter.com/f4bio_sa" >@f4bio_sa</a> no Twitter &rarr;</h2>
-                        <p className="card-text">Apenas acompanho alguns assuntos polêmicos</p>
-                    </div>
-                </div>
+            <div className="feature col">
+                <h6 className="fs-2"><a href="https://www.instagram.com/f4bio.sa/" >@f4bio.sa</a> no Insta&rarr;</h6>
+                <p className="card-text">Um pouco da minha rotina.</p>
             </div>
-
-
-            <div className="col-lg-6 col-xs-12">
-                <div className="card">
-
-                    <div className="card-body">
-                        <h2 className="card-title"><a href="https://www.instagram.com/f4bio.sa/" >@f4bio.sa</a> no Insta&rarr;</h2>
-                        <p className="card-text">Um pouco da minha rotina.</p>
-                    </div>
-                </div>
-            </div>
-            <div className="col-lg-6 col-xs-12">
-                <div className="card">
-                    <div className="card-body">
-                        <h2 className="card-title"> <a href="https://www.linkedin.com/in/f4bio/">/in/f4bio/</a> no LinkedIn &rarr;</h2>
-                        <p className="card-text">Experiência profissional aliada a formação de qualidade!</p>
-                    </div>
-                </div>
-
+            <div className="feature col">
+                <h6 className="fs-2"><a href="https://www.linkedin.com/in/f4bio/">/in/f4bio/</a> no LinkedIn &rarr;</h6>
+                <p className="card-text">Experiência profissional aliada a formação de qualidade!</p>
             </div>
         </div>
     </div>
-    </main>
 </div>
 )
 }
