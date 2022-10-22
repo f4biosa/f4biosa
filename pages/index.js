7184aaf5fd7e71b94d507f665ba7f688b4c71990
Fabio Santos <f4bio.sa@gmail.com>
2022-10-22 13:08:26 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,36 +2,36 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  return (
+    return (
     <div>
       <Head>
         <title>f4bio.dev</title>
         <meta name="description" content="Developer Home Page" />
       </Head>
-      <main className={styles.main}>
-      
-  <div className="container">
-        <div className="row">
-          <div className="col-xs-12 text-align-center">
-              <h1 className={styles.title}>
-                Olá, me chamo Fábio Santos!
-              </h1>
+    <main className={styles.main}>
+        <div className="container">
+            <div className="row">
+          
+              <div className="col-xs-12 text-align-center">
+                  <h1 className={styles.title}>
+                    Olá, me chamo Fábio Santos!
+                  </h1>
 
-              <p className={styles.description}>
-               Esse espaço foi criado para divulgar minhas redes sociais e os trabalhos que realizo como desenvolvedor. 
-              </p>
-              </div>
+                  <p className={styles.description}>
+                   Esse espaço foi criado para divulgar minhas redes sociais e os trabalhos que realizo como desenvolvedor. 
+                  </p>
+                </div>
             </div>
-</div>
- <div class="row">
+          </div>
+        <div className="row">
         <div class="col-lg-6 col-xs-12">
               
                 <a href="https://github.com/f4biosa" className={styles.card}>
                   <h2>/f4biosa no GitHub &rarr;</h2>
                   <p>Aqui você encontra meus repositórios de código-fonte</p>
                 </a>
-</div>
-<div class="col-lg-6 col-xs-12">
+        </div>
+         <div class="col-lg-6 col-xs-12">
                 <a href="https://twitter.com/f4bio_sa" className={styles.card}>
                   <h2>@f4bio_sa no Twitter &rarr;</h2>
                   <p>Apenas acompanho alguns assuntos polêmicos</p>
@@ -52,9 +52,7 @@ export default function Home() {
                 </a>
               </div>
 </div>
-        </div>
-    </div>
-</main>
+       </main>
 </div>
   )
 }
