5aeaf02d460cd517d8c3aa3ce4a9d22ac0b50e5c
Fabio Santos <f4bio.sa@gmail.com>
2022-10-22 13:02:05 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '../styles/bootstrap.css';
 import '../styles/custom.css';
-//import '../styles/globals.css'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
