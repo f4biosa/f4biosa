46e45779e56cc44c1751af6d246c7cdb7883b2fe
Fabio Santos <f4bio.sa@gmail.com>
2022-10-22 13:09:23 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -24,14 +24,14 @@ export default function Home() {
             </div>
           </div>
         <div className="row">
-        <div class="col-lg-6 col-xs-12">
+        <div className="col-lg-6 col-xs-12">
               
                 <a href="https://github.com/f4biosa" className={styles.card}>
                   <h2>/f4biosa no GitHub &rarr;</h2>
                   <p>Aqui você encontra meus repositórios de código-fonte</p>
                 </a>
         </div>
-         <div class="col-lg-6 col-xs-12">
+        <div className="col-lg-6 col-xs-12">
                 <a href="https://twitter.com/f4bio_sa" className={styles.card}>
                   <h2>@f4bio_sa no Twitter &rarr;</h2>
                   <p>Apenas acompanho alguns assuntos polêmicos</p>
@@ -39,13 +39,13 @@ export default function Home() {
               </div>
 
 
-              <div class="col-lg-6 col-xs-12">
+              <div className="col-lg-6 col-xs-12">
                 <a href="https://www.instagram.com/f4bio.sa/" className={styles.card}>
                   <h2>@f4bio.sa no Insta&rarr;</h2>
                   <p>Um pouco da minha rotina.</p>
                 </a>
 </div>
-<div class="col-lg-6 col-xs-12">
+<div className="col-lg-6 col-xs-12">
                 <a href="https://www.linkedin.com/in/f4bio/" className={styles.card}>
                   <h2>/in/f4bio/ no LinkedIn &rarr;</h2>
                   <p>Experiência profissional aliada a formação de qualidade!</p>
